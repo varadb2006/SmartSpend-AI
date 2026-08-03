9367063744ce5f3b73a9b01b49372c1c3ca325b3
Varad Bhagat <varadbhagat2006@gmail.com>
2026-08-03 20:47:39 +0530

    add batch CSV and Excel upload endpoint using pandas

--- backend/app/api/endpoints/transactions.py
+++ backend/app/api/endpoints/transactions.py
@@ -1,5 +1,7 @@
 from typing import List
-from fastapi import APIRouter, Depends
+import pandas as pd
+import io
+from fastapi import APIRouter, Depends,UploadFile, File, HTTPException
 from sqlalchemy.orm import Session
 from app.db.session import get_db
 from app.models.user import User
@@ -21,3 +23,47 @@ def create_transaction( transaction_in : TransactionCreate, db:Session = Depends
 def get_transactions(db: Session = Depends(get_db), current_user:  User = Depends(get_current_user)):
     transactions = db.query(Transaction).filter(Transaction.user_id == current_user.id).all()
     return transactions
+
+@router.post("/upload")
+async def upload_transactions(file: UploadFile = File(...), db : Session = Depends(get_db), current_user : User = Depends(get_current_user)):
+    contents = await file.read()
+    filename = file.filename.lower()
+
+    try:
+        if filename.endswith(".csv"):
+            df = pd.read_csv(io.BytesIO(contents))
+        elif filename.endswith(".xlsx"):
+            df = pd.read_excel(io.BytesIO(contents))
+        else :
+            raise HTTPException(status_code=400, detail="Only .csv and .xlsx filea are allowed")
+
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=f"Error reading file: {str(e)}")
+
+    required_columns = {"Date", "DEscription", "Amount", "Transaction_Type"}
+    if not required_columns.issubset(df.columns):
+        raise HTTPException( status_code=400, detail=f"Missing required columns. File must contain: {', '.join(required_columns)}")
+
+    transaction_Added =0
+    for _, row in df.iterrows():
+        category = row.get("Category", "Uncategorized")
+        if pd.isna(category):
+            category = "Uncategorized"
+
+        db_transaction = Transaction(
+            user_id=current_user.id,
+            transaction_date=str(row["Date"]),
+            description=str(row["Description"]),
+            amount=float(row["Amount"]),
+            transaction_type=str(row["Transaction_Type"]).upper(),
+            category=str(category)
+        )
+        db.add(db_transaction)
+        transactions_added += 1
+
+    db.commit()
+    
+    return {
+        "message": "File processed successfully", 
+        "total_processed": transactions_added
+    }
